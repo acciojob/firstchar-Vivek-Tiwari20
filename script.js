2907013fb43864f19b79764969d32451b7bad965
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-05-03 15:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+// Remove leading and trailing spaces
+str = str.trim();
+
+// Check if the string is empty after removing spaces
+if (str === '') {
+return '';
 }
 
+// Loop through the string and return the first non-space character
+for (let i = 0; i < str.length; i++) {
+if (str[i] !== ' ') {
+return str[i];
+}
+}
+}
 // Do not change the code below
 
 const text = prompt("Enter text:");
